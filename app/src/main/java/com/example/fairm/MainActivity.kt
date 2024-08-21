d5695cdb7b55fb1a102d02c16338e396fb0e2ec7
laiba <laibaasaf@gmail.com>
2024-08-21 00:04:37 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fairm/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.fairm
+
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fairm.ui.theme.FairmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+@Preview
+@Composable
+fun Greeting() {
+    Text(text = "Hello Android")
+}
+
---
+++ app/src/main/java/com/example/fairm/Screen/WelcomeScreen.kt
@@ -0,0 +1,72 @@
+package com.example.fairm.Screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fairm.R
+
+
+@Preview (showBackground = true)
+@Composable
+fun WelcomeScreen()  {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LogoCard()
+        TextChat()
+
+    }
+}
+@Composable
+fun LogoCard() {
+
+    Box (
+        modifier = Modifier
+            .background(Color.White)
+            .shadow(2.dp, shape = RoundedCornerShape(12.dp))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.whatsapp_image_2024_08_20_at_12_46_43_am),
+            contentDescription = "Fairm Logo",
+            modifier = Modifier
+                .padding(12.dp)
+                .size(width = 68.dp, height = 68.dp)
+        )
+
+    }
+}
+@Composable
+fun TextChat() {
+    Box (
+        modifier = Modifier
+            .padding(12.dp)
+            .shadow(2.dp, shape = RoundedCornerShape(12.dp))
+            .background(Color.Green)
+    ) {
+
+        Text(
+            text = "You can start planting seed starting now and will get " +
+                    "get the result on 5 months!",
+            color = Color.White,
+            modifier = Modifier
+                .padding(12.dp)
+        )
+
+    }
+}
